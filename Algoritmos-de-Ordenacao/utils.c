0a2bf3de7690f222bbbfb32106c65ff478fe2bf4
Arthur Aguiar <aguiarthur.32@gmail.com>
2023-03-31 19:13:20 -0300

    Adiciona biblioteca para vetores

---
+++ Algoritmos-de-Ordenacao/utils.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "utils.h"
+
+
+void preenche_vetor(int vetor[], int tamanho){
+	srand(time(NULL));
+
+	for(int i=0; i<tamanho; i++){
+		vetor[i] = rand() % 1001;
+	}
+}
+
+void imprime_vetor(int vetor[], int tamanho){
+	printf("\n[");
+	for(int i=0; i<tamanho-1; i++){
+		printf("%d, ", vetor[i]);
+	}
+	printf("%d]\n", vetor[tamanho-1]);
+}
+
+void le_vetor(int vetor[], int tamanho){
+	for(int i=0; i<tamanho; i++){
+		scanf("%d", &vetor[i]);
+	}
+}
---
+++ Algoritmos-de-Ordenacao/utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+void preenche_vetor(int vetor[], int tamanho);
+
+void imprime_vetor(int vetor[], int tamanho);
+
+void le_vetor(int vetor[], int tamanho);
+
+#endif
